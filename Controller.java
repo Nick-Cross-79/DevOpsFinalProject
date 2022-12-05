b4c8f857208a2a6dee589308b3b411db65334b1a
Nick-Cross-79 <51949764+Nick-Cross-79@users.noreply.github.com>
2022-12-05 13:45:28 -0500

    Delete Controller.java

--- Controller.java
+++
@@ -1,2 +0,0 @@
-//TODO: Create simple controller for java application. controller should declare an unitialized array of json objects. When app starts, app should parse
-//through array and display a button with the "Name" field form json object.
